7e74c338fd9a8d5b6c92745243ba0e0c0842b31a
Eric Dee <atallbirdmaker@protonmail.com>
2021-11-28 18:42:53 -0700

    Working on a very simple JSON message inbox

---
+++ selection_boxes_json/script.js
@@ -0,0 +1,29 @@
+let inbox_1 = {
+  inbox_1: "",
+  message_1: "Hey, give me a call back.",
+  message_2: "What's up? How was your day?",
+  message_3: "Did you get the groceries from the list I sent you?",
+};
+
+let inbox_2 = {
+  inbox_2: "",
+  message_1: "The baby is sick. Please take it to the doctor.",
+  message_2: "I called the mechanic, but they didn't pick up.",
+  message_3: "When was the last time the brewers won a championship?",
+};
+
+let inbox_3 = {
+  inbox_3: "",
+  message_1:
+    "Hello, liked what we saw in your resume. Please select a time and date for a screening call.",
+  message_2:
+    "Bob, this is your boss's new email. Did you finish your report? I could use it early if you did. There's some work I need to get done.",
+  message_3:
+    "I got a hold of the mechanic. They put us in for 1:30pm this Thursday.",
+};
+
+let inboxes = [];
+
+inboxes.push(inbox_1);
+inboxes.push(inbox_2);
+inboxes.push(inbox_3);

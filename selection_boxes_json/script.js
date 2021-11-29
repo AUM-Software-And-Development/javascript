764f8861deb81d1ce3847931c625aab701fe9990
Eric Dee <atallbirdmaker@protonmail.com>
2021-11-28 20:13:05 -0700

    Working very simple json inbox display

--- selection_boxes_json/script.js
+++ selection_boxes_json/script.js
@@ -1,29 +1,35 @@
 let inbox_1 = {
-  inbox_1: "",
+  subject_1: "Call me.",
   message_1: "Hey, give me a call back.",
+
+  subject_2: "How are you?",
   message_2: "What's up? How was your day?",
+
+  subject_3: "Groceries.",
   message_3: "Did you get the groceries from the list I sent you?",
-};
 
-let inbox_2 = {
-  inbox_2: "",
-  message_1: "The baby is sick. Please take it to the doctor.",
-  message_2: "I called the mechanic, but they didn't pick up.",
-  message_3: "When was the last time the brewers won a championship?",
-};
+  subject_4: "Take the baby to the doctor.",
+  message_4: "The baby is sick. Please take it to the doctor.",
 
-let inbox_3 = {
-  inbox_3: "",
-  message_1:
+  subject_5: "The car broke down.",
+  message_5: "I called the mechanic, but they didn't pick up.",
+
+  subject_6: "MLB.",
+  message_6: "When was the last time the brewers won a championship?",
+
+  subject_7: "Your application status.",
+  message_7:
     "Hello, liked what we saw in your resume. Please select a time and date for a screening call.",
-  message_2:
+
+  subject_8: "New email.",
+  message_8:
     "Bob, this is your boss's new email. Did you finish your report? I could use it early if you did. There's some work I need to get done.",
-  message_3:
+
+  subject_9: "Mechanic was scheduled.",
+  message_9:
     "I got a hold of the mechanic. They put us in for 1:30pm this Thursday.",
 };
 
 let inboxes = [];
 
 inboxes.push(inbox_1);
-inboxes.push(inbox_2);
-inboxes.push(inbox_3);
